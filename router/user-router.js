fa80ee641a1021ac9963516f829524a13d799949
yangy <751514836@qq.com>
2020-12-01 16:50:44 +0800

    重置密码完成

--- router/user-router.js
+++ router/user-router.js
@@ -1,9 +1,11 @@
 // 用户模块
 const express = require('express')
+const utility = require('utility')
 const router = express.Router()
 const path = require('path')
 const db = require(path.join(__dirname, '../common.js'))
 
+// 获取用户信息
 router.get('/userinfo', (req, res) => {
     // 1.获取用户标识id
     // 需要从token中反解出用户的id
@@ -29,5 +31,30 @@ router.get('/userinfo', (req, res) => {
     }
 })
 
+
+// 更新密码
+router.post('/updatepwd',async(req, res)=>{
+    // 1.获取参数
+    let id = req.user.id
+    let params = req.body
+    params.oldPwd = utility.md5(params.oldPwd)
+    params.newPwd = utility.md5(params.newPwd)
+
+    // 2.操作数据库
+    let sql = 'update myuser set password = ? where id = ? and password = ?'
+    let ret = await db.operateDb(Sql,[params.newPwd,id,params.oldPwd])
+    // 3.响应状态
+    if(ret && ret.affectedRows > 0){
+        res.json({
+            status:0,
+            message:'重置密码成功'
+        })
+    }else(
+        res.json({
+            status:1,
+            message:'重置密码失败'
+        })
+    )
+})
 
 module.exports = router
